18c8c194a93fceeb2c9f25bd80b89495c38ac7fa
Rohit S <rhtsingh278@yahoo.com>
2019-09-22 17:45:56 -0400

    Board setup done

--- cse308-stratego/src/app/game/game.component.ts
+++ cse308-stratego/src/app/game/game.component.ts
@@ -2,31 +2,10 @@ import {Component, OnInit} from '@angular/core';
 import {Block} from './game-models';
 
 @Component({
-  // tslint:disable-next-line:component-selector
   selector: 'game-page',
   templateUrl: 'game.component.html',
   styleUrls: ['game.component.css']
 
 })
-export class GameComponent implements OnInit{
-
-  ngOnInit() {
-    this.createTable();
-  }
-
-  createTable() {
-    var x=9;
-    var y=9;
-    var chessBoard = document.getElementById("chessBoard");
-    console.log(chessBoard);
-    for (var i=0; i<y; i++) {
-      var row = chessBoard.appendChild(document.createElement("div"));
-      for (var j=0; j<x; j++) {
-        row.appendChild(document.createElement("span"));
-      }
-    }
-  }
-
-
-
+export class GameComponent{
 }
