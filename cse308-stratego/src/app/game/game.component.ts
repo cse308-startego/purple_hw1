22bd119fcd8a5bca80382b49e1de956d998687c3
Rohit S <rhtsingh278@yahoo.com>
2019-09-22 18:58:42 -0400

    TD TR BOARD SETUP

--- cse308-stratego/src/app/game/game.component.ts
+++ cse308-stratego/src/app/game/game.component.ts
@@ -8,5 +8,20 @@ import {Block} from './game-models';
 
 })
 export class GameComponent{
+  columns:string[]=[];
+  rows:number[]=[];
+  ngOnInit() {
+    let gridSize = 10;
+    for (let i=0; i<gridSize;i++){
+      this.columns.push(String.fromCharCode(65+i));
+    }
+    for (let i=0; i<gridSize;i++){
+      this.rows.push(i);
+    }
+  }
+
+  trClick(row,columns){
+    console.log(row,columns)
+  }
 
 }
