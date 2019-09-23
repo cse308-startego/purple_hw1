94e594fda4b00b720a8e81efe3abae4f7292c4d3
Rohit S <rhtsingh278@yahoo.com>
2019-09-23 18:17:38 -0400

    Initializing players

--- cse308-stratego/src/app/game/game-models.ts
+++ cse308-stratego/src/app/game/game-models.ts
@@ -10,3 +10,6 @@ export class Board {
 export class Block {
   filled: Card;
 }
+
+// 1 Marshall 2 General 3 Colonel 4 Major 5 Captain 6 Lieutenant 7 Seargant 8 Miner 9Scout 10 Spy 11 Bomb 12 Flag
+// [1         ,1,       2,          3,     4,           4,          4,        5,      8,      1,      6,      1]
