0da1c703e2e7a2439a3413391495cb46b9955469
Rohit Luthra <rohitluthra9@outlook.com>
2019-09-24 21:06:27 -0400

    updated till rohits pull

--- cse308-stratego/src/app/app.module.ts
+++ cse308-stratego/src/app/app.module.ts
@@ -7,12 +7,14 @@ import {LoginPageComponent} from './login-component/login-page.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatCheckboxModule} from "@angular/material/checkbox";
 import {GameComponent} from './game/game.component';
+import {SignUpComponent} from "./sign-up/sign-up.component";
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginPageComponent,
     GameComponent,
+    SignUpComponent,
   ],
   imports: [
     BrowserModule,
