f6fe2a8d28368ef716802fe9c02a42df9716cede
Rohit Luthra <rohitluthra9@outlook.com>
2019-09-26 18:46:34 -0400

    front end seems ok

--- cse308-stratego/src/app/game/game.component.ts
+++ cse308-stratego/src/app/game/game.component.ts
@@ -8,8 +8,8 @@ import {Block, Card} from './game-models';
 
 })
 export class GameComponent implements OnInit {
-  columns: string[] = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i'];
-  rows: number[] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  columns: number[] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+  rows: number[] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
   public gameBoard: Card[][] = [];
    private redArr: Card[] = [];
