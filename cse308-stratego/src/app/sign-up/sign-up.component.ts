dbc6a9b77c3bd718b09ae0ec2f2b803f197b703a
Rohit S <rhtsingh278@yahoo.com>
2019-09-28 17:46:03 -0400

    JAY + LUTHRA + SINGH

--- cse308-stratego/src/app/sign-up/sign-up.component.ts
+++ cse308-stratego/src/app/sign-up/sign-up.component.ts
@@ -1,4 +1,6 @@
 import {Component, OnInit} from '@angular/core';
+import {Router} from "@angular/router";
+import {ApiService} from "../service/api.service";
 
 @Component({
   selector: 'app-sign-up',
@@ -6,7 +8,36 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./sign-up.component.css']
 })
 export class SignUpComponent implements OnInit {
+  public password;
+  public confirm_password;
   ngOnInit() {
   }
+  constructor(private router: Router, private service: ApiService) { }
 
+  registerUser(event) {
+    event.preventDefault();
+
+    const errors = [];
+    const target = event.target;
+    const fullname = target.querySelector('#fullname').value;
+    console.log(fullname);
+    const email = target.querySelector('#email').value;
+    const password = target.querySelector('#password').value;
+    const cpassword = target.querySelector('#confirm_password').value;
+
+    if(password != cpassword) {
+      confirm("Passwords do not match. Try Again.")
+      errors.push("Passwords do not match")
+    }
+
+    // if(errors.length > 0) {
+    //   this.service.login().subscribe((data: string) => {
+    //     console.log(data);
+    //     if(data.success) {
+    //       this.router.navigateByUrl('/game');
+    //     }
+    //   });
+    // }
+    console.log(email, password)
+  }
 }
